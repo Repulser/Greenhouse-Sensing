5762bcef9a583c5f05aa0b60788ba879522d966c
Repulser <thetechproject2014@gmail.com>
2017-09-30 15:25:24 +0300

    Get endpoints

--- server/server.py
+++ server/server.py
@@ -1,8 +1,8 @@
-from flask import Flask, request
-from pymongo import MongoClient
+import pymongo
+from flask import Flask, request, jsonify
 
 app = Flask(__name__)
-client = MongoClient('localhost', 27017)
+client = pymongo.MongoClient('localhost', 27017)
 db = client.greenhouse
 
 
@@ -10,6 +10,54 @@ def save_ctth(collection, time, temperature, humidity):
     db[collection].insert_one({"time": time, "temperature": temperature, "humidity": humidity})
 
 
+def get_ctth(collection, ammount):
+    return db[collection].find().sort("time", pymongo.DESCENDING).limit(ammount)
+
+
+def generate_json(cursor):
+    object = []
+    for document in cursor:
+        object.append(dict(
+            time=document["time"],
+            temperature=document["temperature"],
+            humidity=document["humidity"]
+        )
+        )
+    return jsonify(object)
+
+
+# Get routes
+@app.route("/current", methods=["GET"])
+def current():
+    current = get_ctth("minutely", 1)[0]
+    return jsonify(
+        temperature=current["temperature"],
+        humidity=current["humidity"],
+        time=current["time"]
+    )
+
+
+@app.route("/hourly", methods=["GET"])
+def hourly():
+    return generate_json(get_ctth("minutely", 60))
+
+
+@app.route("/daily", methods=["GET"])
+def dailyget():
+    return generate_json(get_ctth("daily", 96))
+
+
+@app.route("/weekly", methods=["GET"])
+def weeklyget():
+    return generate_json(get_ctth("weekly", 42))
+
+
+@app.route("/monthly", methods=["GET"])
+def monthlyget():
+    return generate_json(get_ctth("monthly", 30))
+
+
+# Post routes
 @app.route("/ctth", methods=['POST'])
 def minutely():
     time = request.args["time"]
